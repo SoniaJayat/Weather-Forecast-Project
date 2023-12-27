dc0d8c8c5876f30d153d33a0cd1e675866a8b9cd
SoniaJayat <SoniaJayat03@gmail.com>
2023-12-27 14:54:39 +0000

    Injected HTML forecast to JavaScript + edited CSS

--- src/script.js
+++ src/script.js
@@ -57,4 +57,34 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="row">
+    <div class="col-2">
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤</div>
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">
+            <strong>18º</strong>
+          </span>
+          <span class="weather-forecast-temperature-min"> 12º </span>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 searchCity("Paris");
+displayForecast();
